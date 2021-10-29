4f3c3382c7e10a84659fa5589103ea0356f49ff0
ammyy9908 <sb78639@gmail.com>
2021-10-29 11:11:29 +0530

    port 8000 added

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/login', function(req, res) {
 });
 
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT | 8000;
 app.listen(PORT,()=>{
     console.log(`listening at ${PORT}`)
 })
